504406bc748787338d4e46d4975031553457129d
SHAMANTA GARAI <shamanta_gaids2024@msit.edu.in>
2026-03-21 03:06:48 +0530

    eggffd

--- login.js
+++ login.js
@@ -1,16 +1,40 @@
+function setActive(tab){
+    document.getElementById("loginTab").classList.remove("active");
+    document.getElementById("signupTab").classList.remove("active");
+    document.getElementById("adminTab").classList.remove("active");
+
+    tab.classList.add("active");
+}
+
 function showLogin(){
     document.getElementById("loginBlock").style.display = "block";
     document.getElementById("signupBlock").style.display = "none";
+    document.getElementById("adminBlock").style.display = "none";
+    setActive(document.getElementById("loginTab"));
 }
 
 function showSignup(){
     document.getElementById("loginBlock").style.display = "none";
     document.getElementById("signupBlock").style.display = "block";
+    document.getElementById("adminBlock").style.display = "none";
+    setActive(document.getElementById("signupTab"));
+}
+
+function showAdmin(){
+    document.getElementById("loginBlock").style.display = "none";
+    document.getElementById("signupBlock").style.display = "none";
+    document.getElementById("adminBlock").style.display = "block";
+    setActive(document.getElementById("adminTab"));
 }
 
 let loginCaptcha="";
 let signupCaptcha="";
+let adminCaptcha = "";
 
+function generateAdminCaptcha(){
+    adminCaptcha = generateCaptcha();
+    document.getElementById("adminCaptchaText").innerText = adminCaptcha;
+}
 function generateCaptcha(){
     return Math.random().toString(36).substring(2,7).toUpperCase();
 }
@@ -28,6 +52,7 @@ function generateSignupCaptcha(){
 window.onload = function(){
     generateLoginCaptcha();
     generateSignupCaptcha();
+    generateAdminCaptcha();
 };
 
 async function signup(){
@@ -78,5 +103,29 @@ async function login(){
         setTimeout(()=>{
             window.location.href="/dashboard/dashboard.html";
         },1000);
+    }
+}
+
+function adminLogin(){
+
+    const user = document.getElementById("adminUser").value;
+    const pass = document.getElementById("adminPass").value;
+    const captchaInput = document.getElementById("adminCaptchaInput").value;
+    const msg = document.getElementById("authMsg");
+
+    if(captchaInput !== adminCaptcha){
+        msg.innerText = "❌ Invalid CAPTCHA";
+        return;
+    }
+
+    if(user === "admin" && pass === "admin123"){
+        msg.innerText = "✅ Admin Login Successful";
+
+        setTimeout(()=>{
+            window.location.href = "/admin.html";
+        },1000);
+
+    }else{
+        msg.innerText = "❌ Invalid Admin Credentials";
     }
 }
