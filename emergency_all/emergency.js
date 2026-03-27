ed8c019c66182d1ee636e4b63996872ff2078b28
SHAMANTA GARAI <shamanta_gaids2024@msit.edu.in>
2026-03-27 13:40:51 +0530

    eggffd

--- emergency_all/emergency.js
+++ emergency_all/emergency.js
@@ -30,3 +30,13 @@ function updateClock(){
 
 setInterval(updateClock,1000);
 updateClock();
+
+window.onload = function(){
+
+    const map = L.map('map').setView([22.5726, 88.3639], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap',
+    }).addTo(map);
+
+};
